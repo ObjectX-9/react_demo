78050afc3605bd17407ef04bf6fb69802482ca58
zhuling904 <ling.zhu.904@gmail.com>
2024-04-04 23:42:15 +0800

    👨🏻‍💻addLog: 更改commitlint

--- .cz-config.js
+++ .cz-config.js
@@ -1,16 +1,16 @@
 'use strict';
 module.exports = {
 	types: [
-		{value: 'feat', name: '✨新增:    新的内容'},
-		{value: 'fix', name: '🐛修复:    修复一个Bug'},
-		{value: 'docs', name: '📝文档:    变更的只有文档'},
-		{value: 'style', name: '💄格式:    空格, 分号等格式修复'},
-		{value: 'refactor', name: '️♻️重构:    代码重构，注意和特性、修复区分开'},
-		{value: 'perf', name: '️️⚡️性能:    提升性能'},
-		{value: 'test', name: '✅测试:    添加一个测试'},
-		{value: 'build', name: '🔧工具:    开发工具变动(构建、脚手架工具等)'},
-		{value: 'rollback', name: '⏪回滚:    代码回退'},
-		{value: 'addLog', name: '👨🏻‍💻添加log:    代码回退'},
+		{value: '✨feat', name: '✨新增:    新的内容'},
+		{value: '🐛fix', name: '🐛修复:    修复一个Bug'},
+		{value: '📝docs', name: '📝文档:    变更的只有文档'},
+		{value: '💄style', name: '💄格式:    空格, 分号等格式修复'},
+		{value: '♻️refactor', name: '️♻️重构:    代码重构，注意和特性、修复区分开'},
+		{value: '⚡️perf', name: '️️⚡️性能:    提升性能'},
+		{value: '✅test', name: '✅测试:    添加一个测试'},
+		{value: '🔧build', name: '🔧工具:    开发工具变动(构建、脚手架工具等)'},
+		{value: '⏪rollback', name: '⏪回滚:    代码回退'},
+		{value: '👨🏻‍💻addLog', name: '👨🏻‍💻添加log:    增加log'},
 	],
 	scopes: [
 		{name: 'leetcode'},
