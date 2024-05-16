b0588a04ed7281b759357fa2ed7462e96950e1a7
zhuling904 <ling.zhu.904@gmail.com>
2024-05-16 23:19:28 +0800

    feat: grid 实现瀑布流

--- src/components/Waterfall/Waterfall.tsx
+++ src/components/Waterfall/Waterfall.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import type {ReactNode} from 'react';
 import style from './style/index.module.less';
 import {fetchRandomImage} from '../../api';
@@ -43,9 +43,29 @@ const columnTypeRender = (options: WaterfallProps) => {
 
 const flexTypeRender = (options: WaterfallProps) => {
 	const {items = [], maxColumns} = options;
+	const [newMaxColumns, setNewMaxColumns] = useState(maxColumns ?? 5);
+	useEffect(() => {
+		const handleResize = () => {
+			console.log(21321321);
+			if (window.innerWidth < 600) {
+				setNewMaxColumns(2);
+			} else if (window.innerWidth < 800) {
+				setNewMaxColumns(3);
+			} else if (window.innerWidth < 1000) {
+				setNewMaxColumns(4);
+			} else if (window.innerWidth < 1200) {
+				setNewMaxColumns(5);
+			}
+		};
+		window.addEventListener('resize', handleResize);
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
 	return (
 		<div className={style.flexContainer}>
-			{Array(maxColumns)
+			{Array(newMaxColumns)
 				.fill(0)
 				?.map((item, index) => {
 					return (
@@ -70,14 +90,48 @@ const flexTypeRender = (options: WaterfallProps) => {
 
 const gridTypeRender = (options: WaterfallProps) => {
 	const {items = []} = options;
+	// 存储图片高度的状态
+	const [heights, setHeights] = useState(Array(items.length).fill(0));
+
+	// 动态计算图片的高度
+	const calculateHeights = () => {
+		items.forEach((imagesInfo, index) => {
+			const img = new Image();
+			img.src = imagesInfo.urls.full;
+			img.onload = () => {
+				const height = img.offsetHeight;
+				console.log('✅ ~ height:', height);
+				setHeights((prevHeights) => {
+					const newHeights = [...prevHeights];
+					newHeights[index] = height;
+					return newHeights;
+				});
+			};
+		});
+	};
+
+	// 组件挂载后计算图片高度
+	useEffect(() => {
+		calculateHeights();
+	}, [items]);
+
 	return (
-		<div className={style.container}>
+		<div className={style.gridContainer}>
 			{shuffleArray(items)?.map((image: UnsplashImage, index: number) => {
-				console.log('✅ zhuling ~  image:', image);
+				console.log('✅ zhuling ~ image:', image);
 
 				return (
-					<div key={`${image?.id}${index}`} className={style.item}>
-						<img src={image.urls.full} alt={`Image ${index}`} />
+					<div
+						key={`${image?.id}${index}`}
+						className={style.item}
+						style={{
+							gridRowEnd: `span ${Math.ceil(heights[index] / 10) || 1}`, // 计算跨越的行数
+						}}>
+						<img
+							src={image.urls.full}
+							alt={`Image ${index}`}
+							style={{width: '100%', display: 'block'}}
+						/>
 					</div>
 				);
 			})}
